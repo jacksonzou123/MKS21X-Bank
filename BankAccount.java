7e49a9c5ba255ac48c8192f65499a0e912cf889e
Jackson Zou <jzou00@stuy.edu>
2018-10-03 11:00:53 -0400

    Added fields and constructor to BankAccount file

---
+++ BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount{
+	private double balance;
+	private int accountID;
+	private String password;
+
+	public BankAccount(double newbalance, int newaccountID, String newpassword) {
+		balance = newbalance;
+		accountID = newaccountID;
+		password = newpassword;
+	}
+}
